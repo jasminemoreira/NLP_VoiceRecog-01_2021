f2a3fa43cbdc930ee9d0d6c98d91e90aa9706ac3
Jasmine Moreira <jasmine.moreira.2013@gmail.com>
2021-04-09 21:28:55 -0300

    update

--- PLN R e Python/text_mining.py
+++ PLN R e Python/text_mining.py
@@ -9,9 +9,10 @@ import csv
 import PyPDF2
 import string
 import pandas as pd
-from nltk import word_tokenize, BigramCollocationFinder, collocations
+from nltk import word_tokenize, BigramCollocationFinder
 from collections import Counter
-from plotnine import ggplot, geom_col, aes, coord_flip, scales
+from plotnine import ggplot, geom_col, aes, coord_flip
+from gensim import corpora, models
 
 stop_words_path = "C:\\Users\\jasmi\\OneDrive\\Área de Trabalho\\PLN RV\\PLN R e Python\\stopwords.csv"
 document_path = "C:\\Users\\jasmi\\OneDrive\\Área de Trabalho\\PLN RV\\PLN R e Python\\senhor_aneis_1.1.pdf"
@@ -49,26 +50,55 @@ df = pd.DataFrame({
  + coord_flip()
 )
 
-bigram_measures = collocations.BigramAssocMeasures()
-trigram_measures = collocations.TrigramAssocMeasures()
-
 finder = BigramCollocationFinder.from_words(tokens)
-best10 = finder.nbest(bigram_measures.pmi, 10)
 bigram = []
 bfreq = []
 for k,v in finder.ngram_fd.items():
-    
     bigram.append(k[0]+" "+k[1])
     bfreq.append(v)
-    
-df = pd.DataFrame({
-    'words': bigram,
-    'freq': bfreq
-})
 
-(ggplot(df, aes(x='reorder(words, freq)', y='freq', fill='words'))
- + geom_col()
- + coord_flip()
-)
+from nltk.collocations import TrigramCollocationFinder
+trigram = []
+tfreq = []  
+finder = TrigramCollocationFinder.from_words(tokens)
+for k,v in finder.ngram_fd.items():
+    trigram.append(k[0]+" "+k[1]+" "+k[2])
+    tfreq.append(v)
+
+from nltk.collocations import QuadgramCollocationFinder    
+finder = QuadgramCollocationFinder.from_words(tokens)
+quadgram = []
+qfreq = []  
+for k,v in finder.ngram_fd.items():
+    quadgram.append(k[0]+" "+k[1]+" "+k[2]+" "+k[3])
+    qfreq.append(v)
+
+
+dictionary = corpora.Dictionary(tokens)
+
+# convert tokenized documents into a document-term matrix
+corpus = dictionary.doc2bow(tokens)
+
+def plot_bar_x(labels,values):
+    # this is for plotting purpose
+    index = np.arange(len(labels))
+    plt.bar(index, values)
+    plt.xlabel('Word', fontsize=10)
+    plt.ylabel('Beta', fontsize=10)
+    plt.xticks(index, labels, fontsize=10, rotation=70)
+    plt.title('Topic Analysis')
+    plt.show()
+
+# generate LDA model
+ntopics = 2
+ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics=ntopics, id2word = dictionary, passes=20)
+
+print(ldamodel.print_topics(num_topics=ntopics, num_words=4))
+topics = ldamodel.show_topics( num_words=5,formatted=False)
+
+for n,t in topics:
+    words = [i[0] for i in t]
+    betas = [i[1] for i in t]
+    plot_bar_x(words,betas)
 
 
